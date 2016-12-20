f70b8412d40f64e972224bef3ea33f3fc645975e
Jose Amezcua <amezcua.jose29@gmail.com>
2016-12-20 09:48:25 -0800

    finished part 7

--- Project7/project7.js
+++ Project7/project7.js
@@ -1,5 +1,6 @@
 var screen = document.getElementById("screen")
 var namespace = "http://www.w3.org/2000/svg"
+var draw = false
 
 // utility function
 function transformPoint(event) {
@@ -10,13 +11,47 @@ function transformPoint(event) {
   return mousept
 }
 
-// Step 2: drawSquare and drawCircle functions
-function drawSquare(x, y, size, color) {
-  // square drawing code here
+function drawCircle(xpos, ypos, radius, color) {
+  var newcircle = document.createElementNS(namespace,"circle");
+  newcircle.setAttribute("fill",color);
+  newcircle.setAttribute("cx",xpos);
+  newcircle.setAttribute("cy",ypos);
+  newcircle.setAttribute("r",radius);
+  screen.appendChild(newcircle);
+}
+
+function drawSquare(xpos, ypos, size, color) {
+  var newrect = document.createElementNS(namespace,"rect");
+  newrect.setAttribute("fill",color);
+  newrect.setAttribute("x",xpos);
+  newrect.setAttribute("y",ypos);
+  newrect.setAttribute("height", size);
+  newrect.setAttribute("width", size);
+  screen.appendChild(newrect);
 }
 
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
-  // what do you want to do when the user presses down
-  // on the mouse button?
+  draw = true
+})
+
+document.addEventListener("mouseup", function(e) {
+draw = false
+})
+
+document.addEventListener("mousemove", function(e) {
+
+  if(draw == true) {
+  var pt = transformPoint(e)
+  var color = document.getElementById("colorSelect").value
+  var shape = document.getElementById("shapeSelect").value
+  var size = document.getElementById("sizeSelect").value
+
+  if (shape == "square") {
+    drawSquare(pt.x, pt.y, size, color)
+  }
+
+  else {
+    drawCircle(pt.x, pt.y, size, color)
+  }}
 })
